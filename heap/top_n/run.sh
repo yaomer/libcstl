6973e38c37951e9c9c5e3cc7ef3578c5bc6b55fb
妖冶如你 <yaoyeruni@gmail.com>
2019-11-23 19:44:53 +0800

    remove old [sll, stack, queue, heap]

--- heap/top_n/run.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-cc number.c
-./a.out > input
-cc ../heap.c top_n.c
-./a.out < input > output
-rm a.out
-cat output
--- queue/stack/run.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-cc stack.c ../queue_array.c test.c
-./a.out
-rm a.out
