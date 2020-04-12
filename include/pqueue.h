4a996a43ba088fe9fb15fdc284186a7b6521162d
妖冶如你 <yaoyeruni@gmail.com>
2020-04-12 23:14:59 +0800

    fix pqueue(in __down())

--- include/pqueue.h
+++ include/pqueue.h
@@ -6,7 +6,7 @@
 
 typedef struct __pqueue pqueue_t;
 
-typedef bool (*__pqueue_comp_handler)(const void *, const void *);
+typedef bool (*__pq_comp_handler)(const void *, const void *);
 /*
  * bool comp(const void *l, const void *r)
  * {
@@ -14,21 +14,21 @@ typedef bool (*__pqueue_comp_handler)(const void *, const void *);
  *     // return *(int*)l < *(int*)r; ==> 最小优先队列
  * }
  */
-typedef void (*__pqueue_free_handler)(void *);
+typedef void (*__pq_free_handler)(void *);
 
 /* 初始化一个优先队列 */
-pqueue_t *pqueue_init(__pqueue_comp_handler pqcomp, __pqueue_free_handler pqfree);
+pqueue_t *pq_init(__pq_comp_handler pqcomp, __pq_free_handler pqfree);
 /* 判断优先队列是否为空 */
-bool pqueue_empty(pqueue_t *pq);
+bool pq_empty(pqueue_t *pq);
 /* 返回优先队列中存储的元素数 */
-size_t pqueue_size(pqueue_t *pq);
+size_t pq_size(pqueue_t *pq);
 /* 返回堆顶元素 */
-void *pqueue_top(pqueue_t *pq);
+void *pq_top(pqueue_t *pq);
 /* 插入一个元素 */
-void pqueue_push(pqueue_t *pq, void *data);
+void pq_push(pqueue_t *pq, void *data);
 /* 弹出堆顶元素 */
-void pqueue_pop(pqueue_t *pq);
+void pq_pop(pqueue_t *pq);
 /* 释放一个优先队列 */
-void pqueue_free(pqueue_t *pq);
+void pq_free(pqueue_t *pq);
 
 #endif // _LIBCSTL_PQUEUE_H
--- src/pqueue.c
+++ src/pqueue.c
@@ -8,107 +8,99 @@
  * 我们这里用二叉堆来实现优先队列
  */
 
-typedef bool (*__pqueue_comp_handler)(const void *, const void *);
-typedef void (*__pqueue_free_handler)(void *);
+typedef bool (*__pq_comp_handler)(const void *, const void *);
+typedef void (*__pq_free_handler)(void *);
 
 typedef struct __pqueue {
     vector_t *buf;
-    __pqueue_comp_handler pq_comp;
+    __pq_comp_handler pq_comp;
 } pqueue_t;
 
-#define __check_pqueue(pq) (assert(pq))
+#define __check_pq(pq) (assert(pq))
 
 #define __parent(i) (i / 2)
 #define __left(i)   (i * 2)
 #define __right(i)  (i * 2 + 1)
 
-static void __copy(void *des, const void *src)
+static int __pq_comp(pqueue_t *pq, size_t i, size_t j)
 {
-    memcpy(des, &src, sizeof(const void *));
+    return pq->pq_comp(vector_entry(pq->buf, i), vector_entry(pq->buf, j));
 }
 
-static void * __pqueue_get_data(pqueue_t *pq, size_t index)
+static void __up(pqueue_t *pq, size_t i)
 {
-    return *(void**)vector_entry(pq->buf, index);
-}
-
-static bool __pqueue_comp(pqueue_t *pq, size_t i, size_t j)
-{
-    return pq->pq_comp(__pqueue_get_data(pq, i), __pqueue_get_data(pq, j));
-}
-
-/* 维护堆的性质 */
-static void __heap(pqueue_t *pq, size_t i)
-{
-    size_t l = __left(i);
-    size_t r = __right(i);
-
-    if (l > vector_size(pq->buf)) return;
-    size_t o = __pqueue_comp(pq, l, i) ? l : i;
-    if (r > vector_size(pq->buf)) goto next;
-    if (__pqueue_comp(pq, r, o)) o = r;
-next:
-    if (o != i) {
-        vector_swap(pq->buf, i, o);
-        __heap(pq, o);
-    }
-}
-
-pqueue_t *pqueue_init(__pqueue_comp_handler pqcomp, __pqueue_free_handler pqfree)
-{
-    pqueue_t *pq = Calloc(1, sizeof(pqueue_t));
-    pq->buf = vector_init(sizeof(void *));
-    vector_set_copy_handler(pq->buf, __copy);
-    vector_set_free_handler(pq->buf, pqfree);
-    vector_resize(pq->buf, 1); /* heap array下标要求从1开始 */
-    pq->pq_comp = pqcomp;
-    return pq;
-}
-
-bool pqueue_empty(pqueue_t *pq)
-{
-    __check_pqueue(pq);
-    return vector_empty(pq->buf);
-}
-
-size_t pqueue_size(pqueue_t *pq)
-{
-    __check_pqueue(pq);
-    return vector_size(pq->buf) - 1;
-}
-
-void *pqueue_top(pqueue_t *pq)
-{
-    __check_pqueue(pq);
-    return __pqueue_get_data(pq, 1);
-}
-
-void pqueue_push(pqueue_t *pq, void *data)
-{
-    __check_pqueue(pq);
-    vector_push_back(pq->buf, data);
-    size_t i = vector_size(pq->buf) - 1;
     size_t j = __parent(i);
-    while (i > 1 && __pqueue_comp(pq, i, j)) {
+    while (i > 1 && __pq_comp(pq, i, j)) {
         vector_swap(pq->buf, i, j);
         i = j;
         j = __parent(i);
     }
 }
 
-void pqueue_pop(pqueue_t *pq)
+static void __down(pqueue_t *pq, size_t i)
 {
-    __check_pqueue(pq);
+    size_t l = __left(i);
+    size_t r = __right(i);
+    size_t size = vector_size(pq->buf) - 1;
+
+    if (l > size) return;
+    size_t o = __pq_comp(pq, l, i) ? l : i;
+    if (r > size) goto next;
+    if (__pq_comp(pq, r, o)) o = r;
+next:
+    if (o != i) {
+        vector_swap(pq->buf, i, o);
+        __down(pq, o);
+    }
+}
+
+pqueue_t *pq_init(__pq_comp_handler pqcomp, __pq_free_handler pqfree)
+{
+    pqueue_t *pq = Calloc(1, sizeof(pqueue_t));
+    pq->buf = vector_init(sizeof(void *));
+    vector_set_free_handler(pq->buf, pqfree);
+    vector_resize(pq->buf, 1); /* heap array下标要求从1开始 */
+    pq->pq_comp = pqcomp;
+    return pq;
+}
+
+bool pq_empty(pqueue_t *pq)
+{
+    __check_pq(pq);
+    return vector_size(pq->buf) == 1;
+}
+
+size_t pq_size(pqueue_t *pq)
+{
+    __check_pq(pq);
+    return vector_size(pq->buf) - 1;
+}
+
+void *pq_top(pqueue_t *pq)
+{
+    __check_pq(pq);
+    return vector_entry(pq->buf, 1);
+}
+
+void pq_push(pqueue_t *pq, void *data)
+{
+    __check_pq(pq);
+    vector_push_back(pq->buf, data);
+    __up(pq, vector_size(pq->buf) - 1);
+}
+
+void pq_pop(pqueue_t *pq)
+{
+    __check_pq(pq);
     if (vector_size(pq->buf) < 2) return;
     vector_swap(pq->buf, 1, vector_size(pq->buf) - 1);
     vector_pop_back(pq->buf);
-    __heap(pq, 1);
-    return;
+    __down(pq, 1);
 }
 
-void pqueue_free(pqueue_t *pq)
+void pq_free(pqueue_t *pq)
 {
-    __check_pqueue(pq);
+    __check_pq(pq);
     vector_free(pq->buf);
     free(pq);
 }
